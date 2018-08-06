d1eec69c5c226ab88d3d6f7354beb8fe01d17427
yagui <yagui@git.com>
2018-08-06 17:34:28 -0400

    change deamon as service using systemd

--- mazer-service/mazer-service.py
+++ mazer-service/mazer-service.py
@@ -8,6 +8,6 @@ device = I2C.get_i2c_device(0x60)
 while True:
   data=chr(device.readU8(0x01))
   if (data == 'H'):
+    device.writeRaw8(0x02)
     os.system('halt')
-  sleep(10)
-
+  sleep(5) #debug 20 seconds/ running 5 seconds
