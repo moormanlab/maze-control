aa5f48be4d86bd1162ca272e956c5c0cdb1a5c05
Ariel Burman <arielburman@gmail.com>
2018-08-12 15:17:10 -0400

    renamed motors to gates and added single instance for pwm signals

--- modules/adai2c.py
+++ modules/adai2c.py
@@ -8,22 +8,46 @@ import Adafruit_PCA9685
 from multiprocessing import Process, Lock
 
 class AdaI2C(object):
+  class __impl:
+    def __init__(self):
+      # Initialise the PCA9685 using the default address (0x40).
+      self.pwm = Adafruit_PCA9685.PCA9685()
+      # Set frequency to 60hz, good for servos.
+      self.pwm.set_pwm_freq(60)
+      self.lock = Lock()
+      logger.info("initializing adai2c with id %s",id(self))
+      
+    def set(self,number,pwm):
+      logger.debug("setting pwm %s, with value %s, id %s",number,pwm,id(self))
+      #bloquea proceso
+      self.lock.acquire()
+      try:
+         self.pwm.set_pwm(number,0,pwm)
+      finally:
+          # liberar proceso
+          self.lock.release()
+          #pass
+
+  __instance = None
+
   def __init__(self):
-    # Initialise the PCA9685 using the default address (0x40).
-    self.pwm = Adafruit_PCA9685.PCA9685()
-    # Set frequency to 60hz, good for servos.
-    self.pwm.set_pwm_freq(60)
-    self.lock = Lock()
+      """ Create singleton instance """
+      # Check whether we already have an instance
+      if AdaI2C.__instance is None:
+          # Create and remember instance
+          AdaI2C.__instance = AdaI2C.__impl()
+
+      # Store instance reference as the only member in the handle
+      #self.__dict__['_Singleton__instance'] = Singleton.__instance
+
+  def __getattr__(self, attr):
+      """ Delegate access to implementation """
+      return getattr(self.__instance, attr)
+
+  def __setattr__(self, attr, value):
+      """ Delegate access to implementation """
+      return setattr(self.__instance, attr, value)
     
-  def set(self,number,pwm):
-    #bloquea proceso
-    self.lock.acquire()
-    try:
-       self.pwm.set_pwm(number,0,pwm)
-    finally:
-        # liberar proceso
-        self.lock.release()
-        #pass
 
 if __name__ == '__main__':
   import sys,os
@@ -40,6 +64,7 @@ if __name__ == '__main__':
   logger.info('Ada test')
 
   motors = AdaI2C()
+  motors2 = AdaI2C()
   data = [[x,x*10] for x in range(10)]
   procs = []
   for i in data:
---
+++ modules/gates.py
@@ -0,0 +1,257 @@
+# Motor control for maze
+# Author: Ariel Burman
+
+import time
+import logging
+
+logger=logging.getLogger(__name__)
+
+import adai2c as pwm
+# Uncomment to enable debug output.
+#logging.basicConfig(level=logging.DEBUG)
+
+from multiprocessing import Process, Queue, Lock, Value
+
+# motor | open       | close       | key | position
+# 0     | 220 -> 222 | 540 -> 534  | IUL | inner upper left 
+# 1     | 545 -> 535 | 195 -> 205  | IBL | inner bottom left
+# 2     | 210 -> 215 | 555 -> 542  | OBL | outter bottom left
+# 3     | 545 -> 535 | 200 -> 210  | OUL | outter upper left
+# 4     | 520 -> 510 | 220 -> 222  | OBR | outter bottom right
+# 5     | 225 -> 230 | 550 -> 545  | OUR | outter upper right
+# 6     | 195 -> 200 | 500 -> 492  | IBR | inner bottom right
+# 7     | 545 -> 540 | 200 -> 205  | IUR | inner upper right
+
+
+#        /----------------\   /----------------\
+#       /                  \-/                  \
+#      /   /-------------\     /-------------\   \
+#     |   / OUL(3) (0)IUL \   / IUR(7) (5)OUR \   |
+#     |   |               |   |               |   |
+#     |   |               |   |               |   |
+#     |   \ OBL(2) (1)IBL /   \ IBR(6) (4)OBR /   |
+#      \   \-------------/     \-------------/   /
+#       \                  /-\                  /
+#        \----------------/   \----------------/
+
+from ctypes import c_bool,c_int
+
+pwmV = {'IUL': (0,[220, 222, 540, 534]),
+        'IBL': (1,[545, 535, 195, 205]),
+        'OBL': (2,[210, 215, 555, 542]),
+        'OUL': (3,[545, 535, 200, 210]),
+        'OBR': (4,[520, 510, 220, 222]),
+        'OUR': (5,[225, 230, 550, 545]),
+        'IBR': (6,[195, 200, 500, 492]),
+        'IUR': (7,[545, 540, 200, 205]) }
+
+class Motor(object):
+  def __init__(self,index=None):
+    logger.info('Motors %s id %s ',index,id(self))
+    self.index=index
+    self.pwm = pwm.AdaI2C()
+    self._position=Value(c_int,0)
+
+  def setPosition(self,val):
+    self.pwm.set(self.index,val)
+    if val > 0 :
+        self._position.value = int(val)
+
+  def getPosition(self):
+    return int(self._position.value)
+
+
+
+class Gate(object):
+  def __init__(self,name=None,index=None,openGoal=0,openAfter=0,closeGoal=0,closeAfter=0):
+    self.name = name
+    self.openGoal=openGoal
+    self.openAfter=openAfter
+    self.closeGoal=closeGoal
+    self.closeAfter=closeAfter
+    self.motor = Motor(index=index)
+    self._moving = Value(c_bool,False)
+    self.lock = Lock()
+
+  def setMoving(self,val):
+    self._moving.value = bool(val)
+
+  def isMoving(self):
+    return bool(self._moving.value)
+
+  def _moveSlow(self,goal,after):
+    self.lock.acquire()
+    logger.debug('started move Slow %s',self.name)
+    now = self.motor.getPosition()
+    self.setMoving(True)
+    try:
+      if goal > now:
+          diff = 1
+      else:
+          diff = -1
+      #steps = 50
+      #diff = int((goal-now)/steps)
+      #logger.info('Slow gate %s | now %s | goal %s | after %s | diff %s moving %s',gate,now,goal,after,diff,self.motors[key].isMoving())
+      while (goal != now):
+        now += diff
+        logger.info('gate %s now %s',self.name,now)
+        self.motor.setPosition(now)
+        time.sleep(0.002)
+        #self.motors[key].setPosition(now)
+      self.motor.setPosition(goal)
+      time.sleep(0.1)
+      self.motor.setPosition(after)
+      logger.debug('gate %s position after = %s',self.name,self.motor.getPosition())
+    except Exception as e:
+      print (e)
+      self.lock.release()
+    finally:
+      logger.debug('ended move Slow %s',self.name)
+      self.lock.release()
+      self.setMoving(False)
+
+  def _moveFast(self,goal,after):
+    self.lock.acquire()
+    logger.debug('started move Fast %s',self.name)
+    self.setMoving(True)
+    now=self.motor.getPosition()
+    logger.info('Fast gate %s | now %s | goal %s | after %s',self.name,now,goal,after)
+    try:
+      self.motor.setPosition(goal)
+      time.sleep(0.5)
+      self.motor.setPosition(after)
+      logger.debug('gate %s position after = %s',self.name,self.motor.getPosition())
+    except Exception as e:
+      print (e)
+      self.lock.release()
+    finally:
+      logger.debug('ended move Fast %s',self.name)
+      self.lock.release()
+      self.setMoving(False)
+
+  def openGateFast(self):
+    logger.debug('openF %s',self.name)
+    self._moveFast(self.openGoal,self.openAfter)
+
+  def closeGateFast(self):
+    logger.debug('closeF %s',self.name)
+    self._moveFast(self.closeGoal,self.closeAfter)
+
+  def openGate(self):
+    logger.debug('openS %s',self.name)
+    self._moveSlow(self.openGoal,self.openAfter)
+
+  def closeGate(self):
+    logger.debug('closeS %s',self.name)
+    self._moveSlow(self.closeGoal,self.closeAfter)
+
+  def _release(self):
+    logger.debug('release %s',self.name)
+    self.motor.setPosition(0)
+
+
+class MazeGates(object):
+  def __init__(self):
+    # Initialise the PCA9685 using the default address (0x40).
+    # Set frequency to 60hz, good for servos.
+    self.gate = {}
+    self.queue = Queue()
+    for key in pwmV:
+        self.gate[key]=Gate(name=key,index=pwmV[key][0],
+                openGoal=pwmV[key][1][0],openAfter=pwmV[key][1][1],
+                closeGoal=pwmV[key][1][2],closeAfter=pwmV[key][1][3])
+    logger.info('MazerMotors id %s ',id(self))
+
+  def openGateFast(self,key):
+    logger.debug('command openF %s',key)
+    self.queue.put(['of',key])
+
+  def closeGateFast(self,key):
+    logger.debug('command closeF %s',key)
+    self.queue.put(['cf',key])
+
+  def openGate(self,key):
+    logger.debug('command openS %s',key)
+    self.queue.put(['os',key])
+
+  def closeGate(self,key):
+    logger.debug('command closeS %s',key)
+    self.queue.put(['os',key])
+
+  def exit(self):
+    self.queue.put(['exit'])
+
+  def openAll(self):
+    logger.debug('command open all')
+    for key in self.gate:
+      self.openGateFast(key)
+
+  def closeAll(self):
+    logger.debug('command close all')
+    for key in self.gate:
+      self.closeGateFast(key)
+
+  def releaseAll(self):
+    logger.debug('close all')
+    for key in self.gate:
+      self.gate[key]._release()
+
+  def testGates(self):
+    for key in self.gate:
+      self.openGate(key)
+      time.sleep(1)
+      self.closeGate(key)
+      time.sleep(1)
+
+  def run(self):
+    while True:
+      if self.queue.empty() is not True:
+        data = self.queue.get()
+        if data[0] == 'exit':
+          break
+        elif data[0] == 'of':
+          p = Process(target=self.gate[data[1]].openGateFast)
+        elif data[0] == 'cf':
+          p = Process(target=self.gate[data[1]].closeGateFast)
+        elif data[0] == 'os':
+          p = Process(target=self.gate[data[1]].openGate)
+        elif data[0] == 'oc':
+          p = Process(target=self.gate[data[1]].closeGate)
+        p.start()
+      msg = ""
+      for key in self.gate:
+        msg = msg + "M {index} mv = {isit}|".format(index=self.gate[key].name,isit=self.gate[key].isMoving())
+      logger.debug(msg)
+      time.sleep(0.1)
+    
+
+if __name__ == '__main__':
+  import sys,os
+  if not os.path.exists('./logs/'):
+    os.makedirs('./logs/')
+
+  dateformat = '%Y/%m/%d %H:%M:%S'
+  formatter_str = '%(asctime)s.%(msecs)d - %(name)s - %(levelname)s - %(message)s'
+  logfile = 'logs/gates.log'
+
+  logging.basicConfig(filename=logfile,filemode='w+',level=logging.DEBUG,
+          format=formatter_str, datefmt=dateformat)
+
+  logger.info('Gates test')
+  gates = MazeGates()
+  try:
+    p = Process(target=gates.run)
+    p.start()
+    #time.sleep(2)
+    gates.openGateFast('OUL')
+    #time.sleep(3)
+    gates.closeGateFast('OUL')
+    #time.sleep(3)
+    gates.closeAll()
+    #time.sleep(3)
+    gates.testGates()
+    gates.releaseAll()
+    gates.exit()
+    p.join()
+  except:
+    raise NameError ('aaa')
