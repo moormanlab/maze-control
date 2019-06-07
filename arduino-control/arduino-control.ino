992ad4ce71c9e4ba703e8c772d34918018bd2d7f
Ariel Burman <arielburman@gmail.com>
2019-06-07 14:38:17 -0400

    update arduino

--- arduino-control/arduino-control.ino
+++ arduino-control/arduino-control.ino
@@ -1,13 +1,15 @@
 ///////////////////////////////////////////////////////////////////////////////
-// Arduino control for rat maze - V 1.2  2018/10/17
+// Arduino control for rat maze - V 1.3  2019/02/12
 ///////////////////////////////////////////////////////////////////////////////
 #define VERSIONMAY    1
-#define VERSIONMIN    2
+#define VERSIONMIN    3
 
 #include <Wire.h>  // Library which contains functions to have I2C Communication
 #define SLAVE_ADDRESS 0x60 // Define the I2C address to Communicate to Uno
 // i2c uses A4 and A5
 
+// Uncomment next line to see debug messages through USB serial (arduino tty)
+//#define DEBUG
 
 //*****************************************************************************
 // Constants definitions
@@ -47,8 +49,10 @@ enum COMMANDS {
   VALVE_SET_DELAYMULTIDROP, // second byte has multidrop number
   HDMI_ON=16,               // second byte has pin number  0xXXXXABCD A:HDMI1 B:HDMI2 c:HDMI3 d:HDMI4
   HDMI_OFF,                 // second byte has pin number  0xXXXXABCD A:HDMI1 B:HDMI2 c:HDMI3 d:HDMI4
-  IR_LED,                   // second byte set state
-  TRIAL=32,                 // second byte indicates trial type
+  LED,                      // second byte set state
+  TRAINSTART=32,            //
+  TRAINSTOP,                //
+  TRIALSTART,               // second byte indicates trial type
   TEST1=90,                 // 0x5a 01011010
   TEST2=165                 // 0xa5 10100101
 };
@@ -71,6 +75,8 @@ enum COMMANDS {
 #define LED_FAST_FADE_TOP      300
 #define LED_SLOW_FADE_TOP     3000
 
+#define TRIAL_LED_TIME         100
+
 //*****************************************************************************
 // Variables definitions
 //*****************************************************************************
@@ -85,7 +91,14 @@ uint8_t multiDrop;
 uint8_t delayDrop;
 uint16_t delayMultiDrop;
 
-uint32_t timeLap, timeNow;
+uint32_t timeLap, timeNow, timeTLed;
+volatile uint8_t toggleToDo;
+
+#ifdef DEBUG
+volatile bool buttonSerial = false;
+char incomingByte;
+#endif
+
 //*****************************************************************************
 // Pin definitions
 //*****************************************************************************
@@ -105,7 +118,7 @@ const int hdmi2 = 10;  //hdmi 2b
 const int hdmi3 = 12;  //hdmi 1a
 const int hdmi4 = 13;  //hdmi 1b
 
-const int irLed = 8;
+const int trialLed = 8;
 
 //*****************************************************************************
 // Prototypes
@@ -121,15 +134,11 @@ void arduinoReset(void);
 void ledFunction(uint8_t newState);
 bool isButtonPressed();
 void SignalHaltRPI();
-
-//TODO DEBUG
-volatile bool buttonSerial = false;
-char incomingByte;
-
-uint8_t tempcount=0;
+void trialLedToggle(int toggleN);
+void trialLedRun (void);
 
 void loop() {
-  // TODO DEBUG
+#ifdef DEBUG
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
@@ -141,14 +150,17 @@ void loop() {
     Serial.print("R:");
     Serial.println(incomingByte, DEC);
   }
+#endif
   if (isButtonPressed()) attendSystem=true;
   if (attendSystem == true) {
     switch (raspiState) {
       case SYS_OFF:
         timeLap = millis();
         raspiState = SYS_PREBOOT_A;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
+#ifdef DEBUG
+        Serial.print("S:");
+        Serial.println(raspiState,DEC);
+#endif
         break;
       case SYS_PREBOOT_A:
         timeNow = millis();
@@ -156,36 +168,44 @@ Serial.println(raspiState,DEC); //TODO DEBUG
           if (isButtonPressed()) {
             timeLap = millis();
             raspiState = SYS_PREBOOT_B;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
             ledFunction(LED_FAST_BLINK);
+#ifdef DEBUG
+            Serial.print("S:");
+            Serial.println(raspiState,DEC);
+#endif
           }
           else {
             raspiState = SYS_OFF;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
             attendSystem = false;
+#ifdef DEBUG
+            Serial.print("S:");
+            Serial.println(raspiState,DEC);
+#endif
           }
         }
         break;
       case SYS_PREBOOT_B:
-        timeNow = millis();
         if (isButtonPressed()) {
+          timeNow = millis();
           if ((timeNow-timeLap) > WAIT_FOR_PRESSDELAY){
             timeLap = millis();
             raspiState = SYS_BOOTING;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
             ledFunction(LED_FAST_FADE);
             digitalWrite(raspiRun, HIGH);
+#ifdef DEBUG
+            Serial.print("S:");
+            Serial.println(raspiState,DEC);
+#endif
           }
         }
         else {
             raspiState = SYS_OFF;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
             ledFunction(LED_ON);
             attendSystem = false;
+#ifdef DEBUG
+            Serial.print("S:");
+            Serial.println(raspiState,DEC);
+#endif
         }
         break;
       case SYS_BOOTING:
@@ -198,17 +218,21 @@ Serial.println(raspiState,DEC); //TODO DEBUG
           digitalWrite(raspiArduinoLed, HIGH);
           ledFunction(LED_SLOW_BLINK);
           raspiState = SYS_OFF;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
           digitalWrite(raspiRun,LOW);
           attendSystem = false;
+#ifdef DEBUG
+          Serial.print("S:");
+          Serial.println(raspiState,DEC);
+#endif
         }
         break;
       case SYS_RUNNING:
         timeLap = millis();
         raspiState = SYS_PREHALT_A;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
+#ifdef DEBUG
+        Serial.print("S:");
+        Serial.println(raspiState,DEC);
+#endif
         break;
       case SYS_PREHALT_A:
         timeNow = millis();
@@ -216,36 +240,44 @@ Serial.println(raspiState,DEC); //TODO DEBUG
           if (isButtonPressed()) {
             timeLap = millis();
             raspiState = SYS_PREHALT_B;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
             ledFunction(LED_FAST_BLINK);
+#ifdef DEBUG
+            Serial.print("S:");
+            Serial.println(raspiState,DEC);
+#endif
           }
           else {
             raspiState = SYS_RUNNING;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
             attendSystem = false;
+#ifdef DEBUG
+            Serial.print("S:");
+            Serial.println(raspiState,DEC);
+#endif
           }
         }
         break;
       case SYS_PREHALT_B:
-        timeNow = millis();
         if (isButtonPressed()) {
+          timeNow = millis();
           if ((timeNow-timeLap) > WAIT_FOR_PRESSDELAY){
             timeLap = millis();
             raspiState = SYS_HALT_INIT;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
             ledFunction(LED_FAST_FADE);
             SignalHaltRPI(); // sets response for next keep alive message to tell raspi to go off
+#ifdef DEBUG
+            Serial.print("S:");
+            Serial.println(raspiState,DEC);
+#endif
           }
         }
         else {
           raspiState = SYS_RUNNING;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
           ledFunction(LED_SLOW_FADE);
           attendSystem = false;
+#ifdef DEBUG
+          Serial.print("S:");
+          Serial.println(raspiState,DEC);
+#endif
         }
         break;
       case SYS_HALT_INIT:
@@ -257,11 +289,13 @@ Serial.println(raspiState,DEC); //TODO DEBUG
           // and go off
           digitalWrite(raspiArduinoLed, HIGH);
           raspiState = SYS_OFF;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
-Serial.println("error 3");
           ledFunction(LED_SLOW_BLINK);
           digitalWrite(raspiRun,LOW);
+#ifdef DEBUG
+          Serial.print("S:");
+          Serial.println(raspiState,DEC);
+          Serial.println("error 3");
+#endif
         }
         break;
       case SYS_HALTING:
@@ -270,11 +304,13 @@ Serial.println("error 3");
           // after halting time, its safe to go off
           raspiState = SYS_OFF;
           attendSystem = false;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
           ledFunction(LED_ON);
           response = 'C'; 
           digitalWrite(raspiRun,LOW);
+#ifdef DEBUG
+          Serial.print("S:");
+          Serial.println(raspiState,DEC);
+#endif
         }
         break;
       default:
@@ -285,38 +321,24 @@ Serial.println(raspiState,DEC); //TODO DEBUG
 
   if (newRPIPacket == true) {
     if (raspiState == SYS_RUNNING) {
-//digitalWrite(irLed, LOW);
       switch (bufferRX[0]) { // the first byte is the command 
         case RPI_KEEPALIVE:
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
-//digitalWrite(hdmi1a, LOW);
-//digitalWrite(hdmi1b, LOW);
-//digitalWrite(hdmi2a, LOW);
-//digitalWrite(hdmi2b, LOW);
-//if (tempcount % 4 == 0) {
-//  digitalWrite(hdmi1a, HIGH);
-//  digitalWrite(irLed, HIGH);
-//}
-//else if (tempcount % 4 == 1)
-//  digitalWrite(hdmi1b, HIGH);
-//else if (tempcount % 4 == 2) {
-//  digitalWrite(hdmi2a, HIGH);
-//  digitalWrite(irLed, LOW);
-//}
-//else if (tempcount % 4 == 3)
-//  digitalWrite(hdmi2b, HIGH);
-//tempcount +=1;
           response = 'C'; 
+#ifdef DEBUG
+          Serial.print("S:");
+          Serial.println(raspiState,DEC);
+#endif
           break;
         case RPI_HALTING:
           timeLap = millis();
           raspiState = SYS_HALTING;
-	  valveClose(valveRight);
-	  valveClose(valveLeft);
+	        valveClose(valveRight);
+	        valveClose(valveLeft);
           attendSystem = true;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
+#ifdef DEBUG
+          Serial.print("S:");
+          Serial.println(raspiState,DEC);
+#endif
           break;
         case VALVE_OPEN:
           if (bufferRX[1]=='R') valveOpen(valveRight);
@@ -346,18 +368,22 @@ Serial.println(raspiState,DEC); //TODO DEBUG
           if ((bufferRX[1]>0) and (bufferRX[1]<101)) 
             delayMultiDrop = bufferRX[1]*500;
           break;
-        case IR_LED:  // delaydrop should be between 1 and 100
-Serial.print("IR:"); //TODO DEBUG
+        case LED:  // 
+#ifdef DEBUG
+          Serial.print("LED");
+#endif
           if (bufferRX[1]=='H') {
-Serial.println("H"); //TODO DEBUG
-            digitalWrite(irLed, HIGH);
+            digitalWrite(trialLed, HIGH);
+#ifdef DEBUG
+            Serial.println("H");
+#endif
           }
           else if (bufferRX[1]=='L') {
-            digitalWrite(irLed, LOW);
-Serial.println("L"); //TODO DEBUG
+            digitalWrite(trialLed, LOW);
+#ifdef DEBUG
+            Serial.println("L");
+#endif
           }
-          break;
-        case TRIAL:  // delaydrop should be between 1 and 100
           break;
         case HDMI_ON:
           if (bufferRX[1]&0x01)
@@ -379,6 +405,26 @@ Serial.println("L"); //TODO DEBUG
           if (bufferRX[1]&0x08)
             digitalWrite(hdmi4, LOW);
           break;
+        case TRAINSTART:
+          digitalWrite(hdmi1, HIGH);
+          trialLedToggle(5);
+#ifdef DEBUG
+          Serial.println("Training start");
+#endif
+          break;
+        case TRAINSTOP:
+          digitalWrite(hdmi1, LOW);
+          trialLedToggle(5);
+#ifdef DEBUG
+          Serial.println("Training stop");
+#endif
+          break;
+        case TRIALSTART:
+          trialLedToggle(bufferRX[1]);
+#ifdef DEBUG
+          Serial.println("Trial");
+#endif
+          break;
         case TEST1:
           // pulse one second arduino led
           digitalWrite(raspiArduinoLed, HIGH);
@@ -386,7 +432,7 @@ Serial.println("L"); //TODO DEBUG
           digitalWrite(raspiArduinoLed, LOW);
           break;
         case TEST2:
-          int x;
+          uint8_t x;
           x = bufferRX[1];
           while (x) {
             digitalWrite(raspiArduinoLed, HIGH);
@@ -403,17 +449,21 @@ Serial.println("L"); //TODO DEBUG
     }
     else if (raspiState==SYS_HALT_INIT) {
       if (bufferRX[0] = RPI_HALTING){
-       timeLap = millis();
-       raspiState = SYS_HALTING;
-Serial.print("S:"); //TODO DEBUG
-Serial.println(raspiState,DEC); //TODO DEBUG
-       valveClose(valveRight);
-       valveClose(valveLeft);
-       attendSystem = true;
+        timeLap = millis();
+        raspiState = SYS_HALTING;
+#ifdef DEBUG
+        Serial.print("S:");
+        Serial.println(raspiState,DEC);
+#endif
+        valveClose(valveRight);
+        valveClose(valveLeft);
+        attendSystem = true;
       }
       else {
-Serial.println("error 1"); //TODO DEBUG
         // should not happed
+#ifdef DEBUG
+        Serial.println("error 1");
+#endif
       }
     }
     else if (raspiState==SYS_BOOTING) {
@@ -424,11 +474,15 @@ Serial.println("error 1"); //TODO DEBUG
       }
       else {
         // should not happed
-Serial.println("error 2"); //TODO DEBUG
+#ifdef DEBUG
+        Serial.println("error 2");
+#endif
       }
     }
     newRPIPacket = false;
   }
+
+  if (toggleToDo > 0) trialLedRun();
   ledFunction(LED_CONTINUE);
 }
 
@@ -457,18 +511,20 @@ void setup() {
   pinMode(hdmi4, OUTPUT);
   digitalWrite(hdmi4, LOW);
 
-  pinMode(irLed, OUTPUT);
-  digitalWrite(irLed, LOW);
+  pinMode(trialLed, OUTPUT);
+  digitalWrite(trialLed, LOW);
 
   //configure i2c
   Wire.begin(SLAVE_ADDRESS);   // this will begin I2C Connection with 0x40 address
   Wire.onRequest(raspiSendData);    // function called when Pi requests data
   Wire.onReceive(raspiReceiveData); // function called when Pi sends data
-  Serial.begin(9600);  //TODO DEBUG
+#ifdef DEBUG
+  Serial.begin(9600); 
   Serial.print("Version ");
   Serial.print(VERSIONMAY);
   Serial.print('.');
   Serial.println(VERSIONMIN);
+#endif
 
   raspiState = SYS_OFF;
   ledFunction(LED_ON);
@@ -483,6 +539,8 @@ void setup() {
   multiDrop = MULTIDROP;
   delayDrop = DELAYDROP;
   delayMultiDrop = DELAYMULTIDROP;
+
+  toggleToDo = 0;
 }
 
 
@@ -511,34 +569,48 @@ void valveDrop(const int valve) {
 
 void raspiSendData(){
   Wire.write(bufferTX); // return data to PI
-Serial.print('K'); //TODO DEBUG
-Serial.print(bufferTX,DEC); //TODO DEBUG
-Serial.print('\n'); //TODO DEBUG
+#ifdef DEBUG
+  Serial.print('K');
+  Serial.print(bufferTX,DEC);
+  Serial.print('\n');
+#endif
 }
 
 void raspiReceiveData(int numBytes) {
-  Serial.print('T'); //TODO DEBUG
-  Serial.print(numBytes,DEC); //TODO DEBUG
-  Serial.print('|'); //TODO DEBUG
+#ifdef DEBUG
+  Serial.print('T');
+  Serial.print(numBytes,DEC);
+  Serial.print('|');
+#endif
   if (Wire.available()) bufferRX[0] = Wire.read(); //receives first byte -> command 
-  Serial.print(bufferRX[0],DEC); //TODO DEBUG
+#ifdef DEBUG
+  Serial.print(bufferRX[0],DEC);
+#endif
   while(Wire.available()) { // 
     bufferRX[1] = Wire.read();    // receive a byte as character
-    Serial.print('|'); //TODO DEBUG
-    Serial.print(bufferRX[1],DEC); //TODO DEBUG
+#ifdef DEBUG
+    Serial.print('|');
+    Serial.print(bufferRX[1],DEC);
+#endif
   }
   newRPIPacket = true;
   if (bufferRX[0] == RPI_KEEPALIVE) {
     bufferTX = response;
   }
   else bufferTX = 0;
-  Serial.print("\n"); //TODO DEBUG
+#ifdef DEBUG
+  Serial.print("\n");
+#endif
 }
 
 bool isButtonPressed() {
-  //if (digitalRead(powerButton) == LOW) return true;
-  if ((digitalRead(powerButton) == LOW) or (buttonSerial==true)) return true; //TODO DEBUG
+#ifdef DEBUG
+  if ((digitalRead(powerButton) == LOW) or (buttonSerial==true)) return true;
   else return false;
+#else
+  if (digitalRead(powerButton) == LOW) return true;
+  else return false;
+#endif
 }
 
 void SignalHaltRPI() {
@@ -639,6 +711,25 @@ void ledFunction(uint8_t newLedState){
       case LED_OFF:
       default:
         break;
+    }
+  }
+}
+
+
+void trialLedToggle(int toggleN){
+  toggleToDo = toggleN;
+}
+
+void trialLedRun (void){
+  timeNow = millis();
+  if ((timeNow -timeTLed) > TRIAL_LED_TIME) {
+    timeTLed = millis();
+    if (digitalRead(trialLed)==LOW) {
+      digitalWrite(trialLed, HIGH);
+    }
+    else {
+      digitalWrite(trialLed, LOW);
+      toggleToDo -= 1;
     }
   }
 }
